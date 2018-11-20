ffb3d346eee68437907e298a40141b1491917764
Kasper Lauge Madsen <kasperlaugemadsen@gmail.com>
2018-11-20 11:50:42 +0100

    Added ability to change dates dynamically

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
 import { EnergyService } from '../energy.service';
 import { FixedScaleAxis, ILineChartOptions } from 'chartist';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges {
   loading = true;
   labels = ['man','tue','wed', 'thu', 'fri', 'sat', 'sun'];
   series = [
     [10, 6, 7, 8, 5, 4, 6],
     [4,  9, 3, 6, 3, 5, 5]
   ];
+  
+  axisFunc = (value, index) => index % 50 === 0 ? value : null;
 
   options: ILineChartOptions = {
     low: 0,
@@ -21,25 +24,62 @@ export class HomeComponent implements OnInit {
     showPoint: false,
     fullWidth: true,
     axisX: {
-      labelInterpolationFnc: (value, index) => {
-        return index % 100 === 0 ? value : null;
-      }
+      labelInterpolationFnc: this.axisFunc
     }
   };
 
-  constructor(private energyService: EnergyService) { }
+
+  form: FormGroup;
+
+  public get startTime() {
+    return this.form.controls["startTime"].value;
+  }
+  public get endTime() {
+    return this.form.controls["endTime"].value;
+  }
+
+  constructor(private energyService: EnergyService, private fb: FormBuilder) { }
 
   ngOnInit() {
-    const start = new Date("2018-11-18");
-    start.setHours(1);
-    const end = new Date();
+    const et = new Date();
+    const st = new Date(et.toDateString());
+    st.setDate(et.getDate() - 1);
+    this.form = this.fb.group({
+                    endTime: et.toLocaleDateString(),
+                    startTime: st.toLocaleDateString()
+                  });
+
+    const start = new Date(this.startTime);
+    const end = new Date(this.endTime);
+    start.setHours(12);
+    end.setHours(12);
+    this.setAxisFunc();
+    this.getGraphDate(start, end);
+  }
+
+  setAxisFunc() {
+    const diff = new Date(this.endTime - this.startTime).getDate();
+    this.axisFunc = (value, index) => (index % (diff*1)) === 0 ? value : null;
+  }
+
+  onSubmit() {
+    const start = new Date(this.startTime);
+    const end = new Date(this.endTime);
+    start.setHours(12);
+    end.setHours(12);
+    this.setAxisFunc();
+    this.getGraphDate(start, end);
+  }
+
+  getGraphDate(start: Date, end: Date) {
+    this.loading = true;
     this.energyService.getEnergyData(start, end).subscribe(res => {
       // Aggregate data
       const labels = new Array<string>();
       const series1 = new Array<number>();
       const series2 = new Array<number>();
       for (let i = 0; i < res.length; i++) {
-        labels.push(new Date(res[i].timestamp).toLocaleDateString());
+        labels.push(new Date(res[i].timestamp).toLocaleDateString("da-dk"));
         series1.push(res[i].totalGreenEnergy);
         series2.push(res[i].totalEnergy);
       }
@@ -51,8 +91,15 @@ export class HomeComponent implements OnInit {
       this.labels = labels;
       this.loading = false;
     });
+  }
 
+  onChange(val) {
+    console.log(val);
+  }
 
+  ngOnChanges(value) {
+    console.log(this.startTime);
+    console.log(this.endTime);
   }
 
 }
